9486fb28ad00177d6c3914aade4abcfc2782d853
Hati <pohan4630510@gmail.com>
2017-09-17 15:51:49 +0800

    basic

---
+++ db.js
@@ -0,0 +1,80 @@
+var fs   = require('fs')
+
+var blogFile    = './blog.db'
+var accountFile = './account.db'
+    
+var sqlite3 = require('sqlite3').verbose()
+var blogDB  = new sqlite3.Database(blogFile)
+var accountDB  = new sqlite3.Database(accountFile)
+
+function start () {
+    blogDB.serialize(function () {
+        blogDB.run('CREATE TABLE IF NOT EXISTS  table01 (content TEXT,category TEXT)')
+    })
+    accountDB.serialize(function () {
+        accountDB.run('CREATE TABLE IF NOT EXISTS  table01 (username TEXT,password TEXT)')
+    })
+    
+    console.log('db start')
+}
+
+function login (username, password, callback) {
+    var check = false
+    accountDB.serialize(function () {
+        var sql = 'SELECT password FROM table01 WHERE username=?'
+        accountDB.each(sql, [username], function(err, row) {
+            console.log('password :' + row.password)
+            if (row.password === password) {
+                check = true
+                console.log('check true')
+                callback(null, true)
+            }
+            //callback('Username or password is wrong', null)
+            
+        })
+        
+    })
+}
+
+function insert (content, category) {
+    blogDB.serialize(function () {
+        var sql = 'INSERT INTO table01(content,category) VALUES (?,?)'
+        blogDB.run(sql,[content, category])
+    })
+    
+}
+
+function list () {
+    blogDB.serialize(function () {
+        var sql = 'SELECT content,category FROM table01'
+        blogDB.each(sql, function(err, row) {
+            console.log(row.category + ": " + row.content)
+        })
+    })
+}
+
+function list2 (callback) {
+    blogDB.serialize(function () {
+        var sql = 'SELECT content,category FROM table01'
+        blogDB.all(sql, function(err, rows) {
+            console.log(JSON.stringify(rows))
+            callback(JSON.stringify(rows))
+        });
+        
+    })
+}
+
+function createAccount (username, password) {
+    accountDB.serialize(function () {
+        var sql = 'INSERT INTO table01(username,password) VALUES (?,?)'
+        accountDB.run(sql,[username, password])
+    })
+}
+
+
+exports.start  = start
+exports.insert = insert
+exports.list   = list
+exports.list2 = list2
+exports.create = createAccount
+exports.login  = login
---
+++ server.js
@@ -0,0 +1,104 @@
+var express = require('express')
+var db      = require('./db')
+
+var app        = express()
+var session    = require('express-session')
+var FileStore  = require('session-file-store')(session)
+var bodyParser = require('body-parser')
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false})
+
+app.use(session({
+    name: 'name',
+    secret: 'hati',  // 用来对session id相关的cookie进行签名
+    store: new FileStore(),  // 本地存储session（文本文件，也可以选择其他store，比如redis的）
+    saveUninitialized: false,  // 是否自动保存未初始化的会话，建议false
+    resave: false,  // 是否每次都重新保存会话，建议false
+    cookie: {
+        maxAge: 10 * 1000  // 有效期，单位是毫秒
+    }
+}))
+
+app.get('/', function (req, res) {
+    var sess = req.session
+    var name = sess.name
+    if (name == 'b') {
+        console.log('session') 
+    }
+    req.session.name = 'b'
+    
+    res.send('Hello World')
+})
+
+app.get('/list', function (req, res) {
+    db.list2(function (content) {
+        res.send(content)
+    })
+    //res.send()
+})
+
+app.post('/login', urlencodedParser, function (req, res) {
+    var response = {
+        "username" : req.body.username,
+        "password" : req.body.password
+    }
+    console.log(response)
+    db.login(req.body.username, req.body.password, function (err, check) {
+        if (check) {
+            req.session.name = req.body.username    
+            res.redirect('/')
+            console.log(req.body.username + ' login')
+        } else {
+            req.session.name = ''
+            console.log('login failed')
+            res.send('login failed')
+        }
+        
+    })
+    //res.end(JSON.stringify(response))
+    
+})
+
+app.get('/logout', function(req, res) {
+    //req.session.destroy()
+    req.session.name = ''
+    res.redirect('/')
+})
+
+app.post('/create', urlencodedParser, function (req, res) {
+    var response = {
+        "username" : req.body.username,
+        "password" : req.body.password
+    }
+    console.log(response)
+    db.create(req.body.username, req.body.password)
+    res.status(200).json(response)
+})
+
+app.post('/upload', urlencodedParser, function (req, res) {
+    console.log(req.session)
+    var response = {
+        "content"  : req.body.content,
+        "category" : req.body.category  
+    }
+    var sess = req.session
+    var name = sess.name
+    if (name != null) {
+        console.log('session!!')
+        console.log(name + ' upload!!')
+        db.insert(req.body.content, req.body.category)
+        db.list()
+        res.status(200).json(response)
+    } else {
+        console.log(response)
+        res.status(401).json({'err' : 'login failed'})
+        console.log('login failed')
+    }
+    
+    
+})
+
+var server = app.listen(8080, function () {
+    console.log('server start') 
+    db.start()
+})
